75ff7cd260d8fa7a4a72e21774e6668c234d0bc7
bamboPapaya <140327564+bamboPapaya@users.noreply.github.com>
2023-07-23 16:14:18 +0200

    Update paris.php

--- paris.php
+++ paris.php
@@ -82,9 +82,9 @@ footer {
 <section>
   <nav>
     <ul>
-      <li><a href="#">London</a></li>
-      <li><a href="#">Paris</a></li>
-      <li><a href="#">Tokyo</a></li>
+      <li><a href="london.php">London</a></li>
+      <li><a href="paris.php">Paris</a></li>
+      <li><a href="tokyo.php">Tokyo</a></li>
     </ul>
   </nav>
   
@@ -101,3 +101,4 @@ footer {
 
 </body>
 </html>
+
